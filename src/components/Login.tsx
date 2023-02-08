485c40b41aa3602e55887235336ad0fde78ebcb8
LeeDonggu <7sus7@naver.com>
2023-02-08 16:55:11 +0900

    fix error

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -137,7 +137,7 @@ export default function Login() {
   function submitWalletInfoToRN() {
     if (window.ReactNativeWebView) {
       window.ReactNativeWebView.postMessage(
-        JSON.stringify(signData, data as any, address as any, timeStamp as any)
+        JSON.stringify(signData, data as any, address as any)
       );
     }
   }
